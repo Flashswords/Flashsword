1a461521284a1f93f842b836fb1ce998dad10482
szh <947297270@qq.com>
2019-10-21 21:03:30 +0800

    add gcd.c

---
+++ gcd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int gcd(int a,int b)
+{
+	int k,m;
+	if (a<b) 
+	{
+		m=a;
+		a=b;
+		b=m;
+	}
+	if (a%b==0)
+	    k=b; 
+	else
+	    k=gcd(a%b,b);
+    return k;
+}
+main()
+{
+	int a,b,t;
+	scanf("%d%d",&a,&b);
+	t=gcd(a,b);
+	printf("%d\n",t);
+	system("pause");
+} 
